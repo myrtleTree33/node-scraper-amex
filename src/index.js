0279de7b1a56ac74afae2d846a1fe7a73c3984ec
jhtong <me@joeltong.org>
2019-07-27 23:53:51 +0800

    uncommenting unused code

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import util from 'util';
 
 import { scrapeOffers } from './lib/fetcher';
 
-(async () => {
-  const results = await scrapeOffers({});
-  //   console.dir(results);
-  console.log(util.inspect(results, false, null, true /* enable colors */));
-  console.log(results.length);
-})();
+// (async () => {
+//   const results = await scrapeOffers({});
+//   //   console.dir(results);
+//   console.log(util.inspect(results, false, null, true /* enable colors */));
+//   console.log(results.length);
+// })();
 
 const AmexScraper = { scrapeOffers };
 
